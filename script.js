a72398ad3dd2f47f558c244152e266f39dc96811
Dominier <118577440+Dominier@users.noreply.github.com>
2023-04-02 16:11:14 -0700

    Added Month Year to Calendar

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let date = new Date(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
-console.log(date, currYear, currMonth);
+const months = ["January", "February", "March", "April", "May", "June", "July", 
+                "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
-    currentDate.innerText = '${currMonth} ${currYear}';
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
 }
-renderCalender();
+renderCalendar();
