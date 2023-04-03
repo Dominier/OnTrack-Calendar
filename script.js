786eea141a01166c468cd95ef3daa28becd6fabf
Dominier <118577440+Dominier@users.noreply.github.com>
2023-04-02 17:33:09 -0700

    Added self-date on JS

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let date = new Date(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
+
 const months = ["January", "February", "March", "April", "May", "June", "July", 
                 "August", "September", "October", "November", "December"];
 
