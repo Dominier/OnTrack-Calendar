feb883bec09dae6bdd47b45ca6d245f8ccfebd7b
Dominier <118577440+Dominier@users.noreply.github.com>
2023-04-05 19:06:25 -0700

    Showing Next Month First Days

--- script.js
+++ script.js
@@ -13,15 +13,23 @@ const months = ["January", "February", "March", "April", "May", "June", "July",
 const renderCalendar = () => {
     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first of month
     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
     let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i-- ) {
+    for (let i = firstDayofMonth; i > 0; i-- ) { // creating li of previous month last days
         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`; 
     }
 
-    for (let i = 1; i <= lastDateofMonth; i++) {
-        liTag += `<li>${i}</li>`; 
+    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+        // adding active class to li if the current day, month, and year are matched
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+                        && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`; 
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`; 
     }
 
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
