f2f5a7da7fccf0d23a08854ba76e7896cbf05e12
Dominier <118577440+Dominier@users.noreply.github.com>
2023-05-19 18:41:50 -0700

    New Changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Calendar
 const currentDate = document.querySelector(".current-date"),
 daysTag = document.querySelector(".days"),
 prevNextIcon = document.querySelectorAll(".arrowIcons span");
@@ -6,7 +7,7 @@ let date = new Date(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
-//
+
 const months = ["January", "February", "March", "April", "May", "June", "July", 
                 "August", "September", "October", "November", "December"];
 
@@ -54,3 +55,195 @@ prevNextIcon.forEach(icon => {
         renderCalendar();
     });
 });
+
+//First whenever user clicks submit button: store item into a list and save the list locally
+//Make sure the delete will remove the item from this list 
+//Whenever the page gets refreshed first use a for loop for each item in saved list make a new object. 
+
+// TO-DO LIST
+
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || []; // Local storage for todo list -- global variable
+    const nameInput = document.querySelector('#name'); // document.querySelector('#name') is selecting the first 'name' id
+    const newTodoForm = document.querySelector('#new-todo-form');
+
+    const username = localStorage.getItem('username') || ''; // creates local storage for usernames
+
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => { // e is short for event
+    localStorage.setItem('username', e.target.value); // stores username and updates name if changed
+    //requests permission to send notifications
+    
+    DisplayTodos();
+    })
+
+    newTodoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        Notification.requestPermission().then((result) =>{
+            if (result !== 'granted'){
+                alert('You need to allow notifications for your tasks');
+            } else {
+                const timeStamp = new Date().getTime() + 5 * 1000; 
+                
+            }
+        });
+
+        
+        
+        let taskContent = document.querySelector('#content').value;
+
+       
+
+        const todo = {
+            content: taskContent,
+            category: e.target.elements.category.value,
+            timeNotify: e.target.elements.timeStart.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+        todos.push(todo);
+
+        localStorage.setItem('todos', JSON.stringify(todos)); 
+
+        e.target.reset(); // reset the form
+
+        DisplayTodos() // calls function to display the tasks/todos
+    })
+
+    DisplayTodos()
+})
+
+function DisplayTodos() {
+    
+    const todoList = document.querySelector('#todo-list');
+    //localStorage.removeItem("todos");  //removes the items so not as cluttered for now in the local storage need to remove later.
+
+    todoList.innerHTML = "";
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
+
+        const label = document.createElement('label'); // creates new label in html
+        const input = document.createElement('input'); // creates new input in html
+        const span = document.createElement('span'); // creates new span in html
+        const content = document.createElement('div'); // creates new div in html
+        const actions = document.createElement('div'); // creates new div in html
+        const edit = document.createElement('button'); // creates new button in html
+        const deleteButton = document.createElement('button'); // creates new button in html
+        const timeStart = document.createElement('div'); //creates the time if added
+       
+        input.type = 'checkbox'; // check if done or not done
+        input.checked = todo.done; // tell if done or not done
+        span.classList.add('bubble'); // adds bubble span
+
+        timeStart.innerHTML =todo.timeNotify;  //displays the time of the task
+        timeStart.classList.add('time'); //adds a time class
+        
+
+        if (todo.category == 'personal') { // checks if user selects personal or business, then creates span according to choice
+            span.classList.add('personal');
+        } else {
+            span.classList.add('business');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+
+        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`; // writing in html directly               FIX ME NEED TO CHECK HOW HE MAKES THE INNER HTML. 
+
+        edit.innerHTML = 'Edit'; // creates edit text
+        deleteButton.innerHTML = 'Delete'; // creates delete text
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+        todoItem.appendChild(timeStart);
+
+        todoList.appendChild(todoItem);
+
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+        //works now!
+        input.addEventListener('click', e => {
+            todo.done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+            
+            if (todo.done) {
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
+
+            DisplayTodos();
+        })
+
+        /* Edit button */
+        edit.addEventListener('click', e => {
+            console.log(content.querySelector('input'));
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {  //checks if the user unfocuses
+                input.setAttribute('readonly', true); 
+                todo.content = e.target.value;  //changes the content object value to updated value 
+                localStorage.setItem('todos', JSON.stringify(todos));  //updates the localStorage list item and changes it
+                DisplayTodos();
+            })
+        })
+
+        //for each todo object they have an event listener so the deleteButton can call a specific todo element to be deleted
+        //Since this element was "clicked" this element is the 'todo' value. 
+        deleteButton.addEventListener('click', (e) => {
+            console.log(e);     
+           
+			todos = todos.filter(t => t != todo); //filters out an array where the elements do not equal to the specific todo object
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
+        
+    })    
+}
+
+
+
+
+
+
+//experimenting with the notifications
+
+
+//need to find a way to get time 
+//time buttons
+var btn = document.getElementById('btn');
+function amClick(){
+    btn.style.left = '0'
+    
+}
+function pmClick(){
+    btn.style.left = '110px'
+}
+const button = document.querySelector("button");
+/* 
+button.addEventListener("click", () => {
+    Notification.requestPermission().then(perm =>{
+        if (perm === "granted") {
+            if (Math.round(new Date()))
+            new Notification("Example notification", {
+                body: "This is more text"
+                
+            })
+        }
+    })
+})
+*/
