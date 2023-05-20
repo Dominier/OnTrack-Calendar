75840f248281ecad396dbed859f258b49de544b2
SamuelLo <samuello300@gmail.com>
2023-05-19 18:31:01 -0700

    Near final product
    still needs to add notifications but everything should work

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let date = new Date(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
-
+//
 const months = ["January", "February", "March", "April", "May", "June", "July", 
                 "August", "September", "October", "November", "December"];
 
